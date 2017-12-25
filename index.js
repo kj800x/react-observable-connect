91253d6985e1184100fe8bc4e1899d2f49c3ac38
Kevin Johnson <kevin@coolkev.com>
2017-12-25 02:07:53 -0500

    Brought it to a usable state Check to see properties have changed before setting the state to prevent unnecessary renders. Added deep-equals as a dependency to do so. Extracted a functional prop mapping in order to do this. Allowed props passed to the smart component to be passed onto the dumb component

---
+++ index.js
@@ -0,0 +1,4 @@
+import connect from './src/connect';
+
+// noinspection JSUnusedGlobalSymbols - this is the library export
+export default connect;
---
+++ src/connect.js
@@ -0,0 +1,111 @@
+import React, {Component} from 'react';
+import deepEqual from 'deep-equal';
+
+/**
+ * Merge objects a and b and return a new object with the merged result. If a and b both have the same key, the value for b will be used in the result.
+ */
+function mergeObjects(a, b) {
+  const result = {};
+  const aKeys = Object.keys(a);
+  const bKeys = Object.keys(b);
+  for (let i = 0; i < aKeys.length; i++) {
+    result[aKeys[i]] = a[aKeys[i]];
+  }
+  for (let i = 0; i < bKeys.length; i++) {
+    result[bKeys[i]] = b[bKeys[i]];
+  }
+  return result;
+}
+
+/** Connect the state of an observableObject to the props of a react component (via a HOC's state).
+ *
+ * The observableObject must expose a subscribe method which takes in a callback which is called whenever the observableObject's state changes.
+ * The observableObject's subscribe method should return an unsubscribe method which can be called to unsubscribe the callback from the state changes.
+ *
+ * mapObjectToValueProps should take in the observableObject and return an object. The object should be the value (non-functional) props that the
+ * dumb component receives. The return value of this should be able to be compared by react to determine if the dumb component needs to be re-rendered.
+ *
+ * mapObjectToFunctionProps should take in the observableObject and return an object. The object should be the function props that the dumb component receives.
+ * Important! The return value of this function will not be compared to check for required state updates, so if you return a freshly bound version of the
+ * functions, that is ok. As a consequence, this function's return value should not change unless the return value of mapObjectToValueProps changes, as this
+ * will only be recomputed if the return value of mapObjectToValueProps changes.
+ *
+ *
+ * @param observableObject an object that has a method named subscribe with the signature: (function callback) -> (function unsubscribe).
+ *                         if the observableObject's state changes, then the callback function should be called.
+ *                         if the unsubscribe function is called, then the observableObject should remove references to the callback and stop calling it.
+ *
+ * @param mapObjectToValueProps
+ *                         a function that turns the observableObject into a set of props for the DumbComponent. These props should only be values
+ *                         (comparable for equality).
+ *
+ * @param mapObjectToFunctionProps
+ *                         a function that turns the observableObject into a set of props for the DumbComponent. These props should only be functions.
+ *
+ * @returns {Function}     a function which takes one argument, the DumbComponent and returns a SmartComponent by passing the props from the observableObject
+ *                         into it.
+ */
+export default function(observableObject, mapObjectToValueProps, mapObjectToFunctionProps) {
+
+  return function(DumbComponent) {
+
+    class SmartComponent extends Component {
+
+      constructor(props) {
+        super(props);
+        this.state = {
+          valueProps: {}
+        };
+        this.unsubscribe = null;
+      }
+
+      trySubscribe() {
+        if (!this.unsubscribe) {
+          this.unsubscribe = observableObject.subscribe(this.handleChange.bind(this));
+          this.handleChange();
+        }
+      }
+
+      tryUnsubscribe() {
+        if (this.unsubscribe) {
+          this.unsubscribe();
+          this.unsubscribe = null
+        }
+      }
+
+      componentDidMount() {
+        this.trySubscribe()
+      }
+
+      componentWillUnmount() {
+        this.tryUnsubscribe();
+      }
+
+      handleChange() {
+        if (!this.unsubscribe) {
+          return
+        }
+
+        const newValueProps = mapObjectToValueProps(observableObject);
+        if (!deepEqual(newValueProps, this.state.valueProps)) {
+          this.setState({valueProps: newValueProps});
+        }
+      }
+
+      render() {
+        console.log("rerender caused");
+
+        // Merge this element's props, the value props, and the event listener props all into one allProps.
+        const allProps = mergeObjects(mergeObjects(this.props, this.state.valueProps), mapObjectToFunctionProps(observableObject));
+
+        // Create a DumbComponent using allProps
+        return React.createElement(DumbComponent,
+                                   allProps);
+      }
+    }
+
+    return new SmartComponent();
+
+  }
+
+}
