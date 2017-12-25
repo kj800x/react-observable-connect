a33340aa677423188ae2c388218c5031940b01aa
Kevin Johnson <kevin@coolkev.com>
2017-12-25 14:28:06 -0500

    Added necessary test framework dependencies and a sample test to confirm that they're working

---
+++ src/connect.test.js
@@ -0,0 +1,11 @@
+import assert from "assert";
+
+describe("xxx", function() {
+  describe("yyy", function () {
+    it("should be zzz", function() {
+      assert.equal("hi", "hi");
+      assert.notEqual(1, 2);
+    });
+  });
+});
+
